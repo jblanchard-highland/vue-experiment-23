67b96587a7a0dcd60d41e1cc7b7e0342c1855369
Jeff Blanchard <jblanchard@highlandsolutions.com>
2023-09-06 08:46:55 -0500

    Revert "Update checklist-render.js"
    This reverts commit 82c8253a763cac5efe4f3793547bfad02b812403.

--- checklist-render.js
+++ checklist-render.js
@@ -16,20 +16,15 @@ methods: {
     showItemsWithDelay() {
         this.items.forEach((item, index) => {
             setTimeout(() => {
-                item.id = index + 1;
-                item.visible = true;
-                
-                // Force Vue to immediately recognize the change and update the DOM
-                this.$nextTick(() => {
-                    console.log('Item rendered:', item.text);
-                });
+                item.id = index;
+		item.visible = true;
             }, index * 500); // 500ms delay between each item
         });
     }
 },
     template: `
       <form id="checklist-wrapper">
-        <div v-for="item in items" :key="item.id" class="checklist-item" v-if="item.visible">
+        <div v-for="item in items" :key="item.id" class="checklist-item">
           <label :for="'item-' + item.id" class="w-checkbox checklist-item-label">
 	          <input class="w-checkbox-input" type="checkbox" v-model="item.checked" :id="'item-' + item.id">
   	        <span class="checklist-item-label-text w-form-label">{{ item.text }}</span>
